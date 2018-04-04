46e81450aa7b09c2d1a1170682f192c8ce129991
richj <richj@oregonstate.edu>
2018-04-03 22:02:41 -0700

    Program 2 done

--- Program2.c
+++ Program2.c
@@ -24,7 +24,7 @@ void generate(struct student* students){
      /*Generate random initials and scores for ten students.
 	The two initial letters must be capital and must be between A and Z.
 	The scores must be between 0 and 100*/
-  for (size_t i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i++) {
     students[i].initials[0] = rand()%(90+1 - 65)+65;//ASCII code for a capatal letter
     students[i].initials[1] = rand()%(90+1 - 65)+65;
     students[i].score = rand()%100;
@@ -38,7 +38,7 @@ void output(struct student* students){
               2. Initials  Score
               ...
               10. Initials Score*/
-		for (size_t i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) {
 			printf("%d", i);
 			printf("%s", " ");
 			printf("%s", (char*) students[i].initials);
@@ -49,17 +49,40 @@ void output(struct student* students){
 
 void summary(struct student* students){
      /*Compute and print the minimum, maximum and average scores of the ten students*/
+		 int max = students[0].score;
+ 		 int min = students[0].score;
+ 		 int avg = 0;
 
+
+
+		 for (size_t i = 0; i < 10; i++) {
+			 avg += students[i].score;
+			 if (students[i].score > max) {
+				 	max = students[i].score;
+			 }
+			 if (students[i].score < min) {
+				 	min = students[i].score;
+			 }
+		 }
+		 avg = avg/10;
+		 printf("%s\n", "--------");
+		 printf("%s", "Max: ");
+		 printf("%d\n", max);
+		 printf("%s", "Min: ");
+		 printf("%d\n", min);
+		 printf("%s", "Avg: ");
+		 printf("%d\n", avg);
 }
 
 void deallocate(struct student* stud){
      /*Deallocate memory from stud*/
-		 for (size_t i = 0; i < 10; i++) {
-		 	free(stud[i]);
+		 if (stud != NULL) {
+			 	free(stud);
 		 }
 		 if(stud == NULL){
 			 printf("%s\n", "Good to go");
 		 }
+
 }
 
 int main(){
--- Program3.c
+++ Program3.c
@@ -3,25 +3,38 @@
  * Date:
  * Solution description:
  */
- 
+
 #include <stdio.h>
+#include<stdlib.h>
 
 void sort(int* number, int n){
-     /*Sort the array of integeres of length n*/     
+     /*Sort the array of integeres of length n*/
 }
 
 int main(){
+    time_t t;
+    srand((unsigned) time(&t));
     /*Declare an integer n and assign it a value of 20.*/
-    
+    int n = 20;
     /*Allocate memory for an array of n integers using malloc.*/
-    
+    int *arr;
+    if (n > 0) {
+      arr = (int*) malloc(n*sizeof(int));
+    }
     /*Fill this array with random numbers, using rand().*/
-    
+    for (int i = 0; i < n; i++) {
+      arr[i] = rand()%100;
+    }
     /*Print the contents of the array.*/
-
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr[i]);
+    }
     /*Pass this array along with n to the sort() function of part a.*/
-    
-    /*Print the contents of the array.*/    
-    
+    sort(arr,n);
+    /*Print the contents of the array.*/
+    printf("%s\n", "--------");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr[i]);
+    }
     return 0;
 }
